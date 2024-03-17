31374c1f4867995ac291bc5a5cbfb33d85dde550
Alireza <alireza.bayrami@gmail.com>
2024-03-17 12:57:11 +0330

    Posts & Comments models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "Posts"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return f"{self.title} -> {self.content}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    text = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.email} -> {self.text}"
+
+    class Meta:
+        db_table = "Comments"
+        verbose_name_plural = "Comments"
