9799f91ac86ecd07634052f1509bc211e385c88c
Alireza <alireza.bayrami@gmail.com>
2024-03-18 15:21:06 +0330

    commenting url changed

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from blog.Views.comments_views import CommentsViewSet
 
 router = DefaultRouter()
-router.register("all_post", PostsViewSet, basename="posts-api")
+router.register("posts", PostsViewSet, basename="posts-api")
 router.register("comments", CommentsViewSet, basename="comments-api")
 urlpatterns = router.urls
