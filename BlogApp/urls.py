8e591fc1a2965dcef4eb6e4306cac31eb6ba0ec5
Alireza <alireza.bayrami@gmail.com>
2024-03-29 14:36:37 +0330

    index app added

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("register/", include("user.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include("index.urls")),
 ]
